cbe55301bdf3b8a5d24097ebda927bcb03d8d6d8
katywoman <105601730+katywoman@users.noreply.github.com>
2022-05-15 19:43:47 +0200

    prvo nalaganje

---
+++ data/activity_3987881301waypoints_1.js
@@ -0,0 +1,1 @@
+var json_activity_3987881301waypoints_1 = {"type":"FeatureCollection","name":"activity_3987881301waypoints_1","crs":{"type":"name","properties":{"name":"urn:ogc:def:crs:OGC:1.3:CRS84"}},"features":[]}
